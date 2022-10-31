a3ae631d9ea9c4ade851b3ca933898487d9ed16b
Siddharth Raghunath Rai <siddharthrai29@gmail.com>
2022-10-30 19:47:25 -0700

    Update FibonacciUpToNTerms.c
    updated input

--- oct2021/FibonacciUpToNTerms.c
+++ oct2021/FibonacciUpToNTerms.c
@@ -2,10 +2,10 @@
 void main() 
 {
     int i, n, a= 0, b= 1, sumab;
-    printf("Fibonacci Series up to n terms: ");
+    printf("Fibonacci Series till n terms: ");
     printf(" Enter the number");
     scanf("%d", &n);
-    printf("Fibonacci Series up to n terms:");
+    printf("Fibonacci Series up till n terms:");
 
     for (i = 0; i <= n; i++) 
     {
